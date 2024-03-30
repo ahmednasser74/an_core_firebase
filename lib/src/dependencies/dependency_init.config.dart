954abf56eadfd02c63b8f716bee93f593e3fb97c
nasser <a.nasser@obeikan.com.sa>
2024-03-31 00:07:15 +0200

    modifications for register module

--- lib/src/dependencies/dependency_init.config.dart
+++ lib/src/dependencies/dependency_init.config.dart
@@ -8,14 +8,11 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:firebase_analytics/firebase_analytics.dart' as _i4;
-import 'package:firebase_core/firebase_core.dart' as _i6;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
-import '../firebase/firebase_analytics_helper.dart' as _i5;
+import '../firebase/firebase_analytics_helper.dart' as _i4;
 import '../firebase/notification_service.dart' as _i3;
-import 'register_module.dart' as _i7;
 
 // initializes the registration of main-scope dependencies inside of GetIt
 _i1.GetIt $initGetIt(
@@ -28,12 +25,7 @@ _i1.GetIt $initGetIt(
     environment,
     environmentFilter,
   );
-  final registerModule = _$RegisterModule();
   gh.factory<_i3.AppNotificationService>(() => _i3.AppNotificationService());
-  gh.singleton<_i4.FirebaseAnalytics>(registerModule.firebaseMessaging());
-  gh.factory<_i5.FirebaseAnalyticsHelper>(() => _i5.FirebaseAnalyticsHelper());
-  gh.singletonAsync<_i6.FirebaseApp>(() => registerModule.firebase());
+  gh.factory<_i4.FirebaseAnalyticsHelper>(() => _i4.FirebaseAnalyticsHelper());
   return getIt;
 }
-
-class _$RegisterModule extends _i7.RegisterModule {}
--- lib/src/dependencies/register_module.dart
+++ lib/src/dependencies/register_module.dart
@@ -1,12 +1,12 @@
-import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:injectable/injectable.dart';
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_analytics/firebase_analytics.dart';
+// import 'package:injectable/injectable.dart';
+// import 'package:firebase_core/firebase_core.dart';
 
-@module
-abstract class RegisterModule {
-  @singleton
-  FirebaseAnalytics firebaseMessaging() => FirebaseAnalytics.instance;
+// @module
+// abstract class RegisterModule {
+//   @singleton
+//   FirebaseAnalytics firebaseMessaging() => FirebaseAnalytics.instance;
 
-  @singleton
-  Future<FirebaseApp> firebase() => Firebase.initializeApp();
-}
+//   @singleton
+//   Future<FirebaseApp> firebase() => Firebase.initializeApp();
+// }
